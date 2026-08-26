45e9be853dc8466f69e284e60ea28f521a7d5fe2
SilasBuildsUselessStuff <silashirning@gmail.com>
2026-08-26 23:45:51 +0200

    Add central configuration for BMO Companion App

---
+++ python/config.py
@@ -0,0 +1,18 @@
+"""Central configuration for the BMO Companion App."""
+
+# Serial connection
+SERIAL_PORT = "COM3"
+BAUDRATE = 115200
+SERIAL_TIMEOUT = 0.2
+
+# BMO identity
+BMO_NAME = "BMO"
+
+# Physical BMO display dimensions
+BMO_WIDTH = 480
+BMO_HEIGHT = 320
+
+# Development GUI dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+WINDOW_TITLE = "BMO Companion App - Development"
